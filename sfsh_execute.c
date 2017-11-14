b7eec7de888cfc2ea5d54e6bf1b8938cca023f0d
Henry Hsu <h_hsu817@yahoo.com>
2017-11-14 21:55:35 +0000

    implements execute function

--- sfsh_execute.c
+++ sfsh_execute.c
@@ -3,7 +3,29 @@
 
 int sfsh_execute(char **args)
 {
+	int status = 1, i = 0;
 
+	typedef struct builtin
+	{
+		char *name;
+		int (*function)(char **);
+	} builtin_t;
+
+
+	builtin_t builtins[] = {
+		{"help", hsh_help},
+		{"exit", hsh_exit}
+	};
+
+	for (i = 0; i < 2; i++)
+	{
+		if (strcmp(args[0], builtins[i].name) == 0)
+		{
+			return (builtins[i].function(args));
+		}
+
+
+	}
 
 	return (0);
 }
