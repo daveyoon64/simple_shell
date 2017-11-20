b7ee0a7bffda25c7cc46144f8906b461c6d9d42a
Henry Hsu <h_hsu817@yahoo.com>
2017-11-20 23:23:47 +0000

    wip

--- sfsh_builtins1.c
+++ sfsh_builtins1.c
@@ -9,13 +9,53 @@
  */
 int sfsh_cd(char **args)
 {
-	if (chdir(args[1]) != 0)
+	int i = 0;
+	char *target;
+
+	while(args[++i])
+		;
+
+	if (i < 2)
 	{
-		perror("");
+		i = 0;
+		while (environ[i])
+		{
+			if (_strncmp("HOME=", environ[i], 5) == 0)
+			{
+				target = environ[i];
+				break;
+			}
+			i++;
+		}
+		target += 5;
+
+		if (chdir(target) != 0)
+		{
+			perror("");
+		}
 	}
+	else
+	{
+		printf("change to specified dir\n");
+		if (chdir(args[1]) != 0)
+		{
+			perror("");
+		}
 
+	}
+/*
+	printf("i is %d\n", i);
+	args[1] = "HOME";
+	args[2] = target;
+	args[3] = "1";
+	args[4] = '\0';
 
-
+	for (i = 0; args[i] != '\0'; i++)
+	{
+		printf("value: %s\n", args[i]);
+	}
+	sfsh_setenv(args);
+*/	
 	return (1);
 }
 /**
@@ -29,19 +69,17 @@ int sfsh_cd(char **args)
  */
 int sfsh_setenv(char **args)
 {
-	int i = 0, length = 0;
-	char *new_env;
+	int i = 0, length = 0, new_entry = 0;
+	char *new_value;
 
 	/* check for proper number of arguments, else return */
 	while (args[++i])
 		;
-
 	if (i != 4 || args[3] == 0)
 	{
 		printf("Wrong arg count\n");
 		return (1);
 	}
-
 	/* ensure no '=' for proper syntax processing */
 	for (length = 0; args[1][length] != '\0'; length++)
 	{
@@ -51,22 +89,25 @@ int sfsh_setenv(char **args)
 			return (1);
 		}
 	}
-
-	new_env = malloc(_strlen(args[0]) + _strlen(args[1]) + 2);
-	_strcpy(new_env, args[1]);
-	_strcpy(&new_env[_strlen(new_env)], "=");
-	_strcpy(&new_env[_strlen(new_env)], args[2]);
-
+	new_value = malloc(_strlen(args[0]) + _strlen(args[1]) + 2);
+	_strcpy(new_value, args[1]);
+	_strcpy(&new_value[_strlen(new_value)], "=");
+	_strcpy(&new_value[_strlen(new_value)], args[2]);
 	i = 0;
 	while (environ[i])
 	{
 		if (_strncmp(args[1], environ[i], length) == 0)
 		{
-			environ[i] = new_env;
+			environ[i] = new_value;
+			new_entry = 1;
 		}
 		i++;
 	}
-
+	if (new_entry == 0)
+	{
+		environ[i++] = new_value;
+		environ[i] = NULL;
+	}
 	return (1);
 }
 /**
@@ -115,6 +156,7 @@ int sfsh_unsetenv(char **args)
 		i++;
 		j++;
 	}
+
 	environ = new_env;
 	return (1);
 }
